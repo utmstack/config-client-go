ab174175416474398763703add1715164985dfc1
Osmany Montero <osmontero@icloud.com>
2024-02-12 15:58:53 +0200

    updating library

--- client.go
+++ client.go
@@ -1,4 +1,4 @@
-package UTMStackConfigurationClient
+package utmconfig
 
 import (
 	"bytes"
--- client_test.go
+++ client_test.go
@@ -1,10 +1,10 @@
-package UTMStackConfigurationClient_test
+package utmconfig_test
 
 import (
 	"fmt"
 	"testing"
 
-	UTMStackConfigurationClient "github.com/utmstack/config-client-go"
+	utmconfig "github.com/utmstack/config-client-go"
 	"github.com/utmstack/config-client-go/enum"
 )
 
@@ -12,12 +12,12 @@ func TestClient(t *testing.T) {
 	for {
 		confKey := "qNANPjjNm7eantt7sgld0iSWFFeGKz5i"
 		utmPanel := "https://192.168.1.18"
-		client := UTMStackConfigurationClient.NewUTMClient(confKey, utmPanel)
+		client := utmconfig.NewUTMClient(confKey, utmPanel)
 		config, err := client.GetUTMConfig(enum.O365)
 		if err != nil {
 			t.Errorf("Error %s", err.Error())
 		}
 		fmt.Println(config)
 	}
-	
+
 }
