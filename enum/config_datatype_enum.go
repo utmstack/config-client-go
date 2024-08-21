1ad0f15738ba28352880afe0e29dcfbe0c5db6e6
Yorjander Hernandez Vergara <yorjaKbayero@gmail.com>
2024-08-20 23:20:28 -0400

    Update Library

--- enum/config_datatype_enum.go
+++ enum/config_datatype_enum.go
@@ -4,10 +4,10 @@ type UTMConfigDataType string
 
 const (
 	TextType     UTMConfigDataType = "text"
-	TelType                        = "tel"
-	PasswordType                   = "password"
-	EmailType                      = "email"
-	NumberType                     = "number"
-	BooleanType                    = "bool"
-	ListType                       = "list"
+	TelType      UTMConfigDataType = "tel"
+	PasswordType UTMConfigDataType = "password"
+	EmailType    UTMConfigDataType = "email"
+	NumberType   UTMConfigDataType = "number"
+	BooleanType  UTMConfigDataType = "bool"
+	ListType     UTMConfigDataType = "list"
 )
